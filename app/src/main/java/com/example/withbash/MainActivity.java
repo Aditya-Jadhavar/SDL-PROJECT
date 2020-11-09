005a1da8ba046796a6257d193a51ebb12832b762
Aditya <jadhavaraditya9@gmail.com>
2020-11-09 12:20:53 +0530

    Created Login System and Bottom Navigation

--- app/src/main/java/com/example/withbash/MainActivity.java
+++ app/src/main/java/com/example/withbash/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.withbash;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        navController = Navigation.findNavController(this,R.id.frame_layout);
+
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
+
     }
 }
