adda3ff3710ce532724695871400a3b8dc5e5c40
AbhishekJadhav17 <54761682+AbhishekJadhav17@users.noreply.github.com>
2020-12-15 16:33:29 +0530

    Changes in Navigations pay to cart

--- app/src/main/java/com/example/withbash/ui/pay/Pay.java
+++ app/src/main/java/com/example/withbash/ui/pay/Pay.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,8 +16,10 @@ import android.widget.Button;
 import com.example.withbash.R;
 
 
-public class Pay extends Fragment {
+public class Pay extends Fragment implements View.OnClickListener {
 
+    private NavController navController;
+    public Button button;
 
 
     public Pay() {
@@ -34,8 +37,17 @@ public class Pay extends Fragment {
 
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController= Navigation.findNavController(view);
+        button =view.findViewById(R.id.pay_nav);
+        button.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        navController.navigate(R.id.action_navigation_pay_to_cart);
 
-
-
+    }
 }
