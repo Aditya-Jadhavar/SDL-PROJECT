6257b4f83a7b2b078096a5c398794d7abf20aa25
AbhishekJadhav17 <54761682+AbhishekJadhav17@users.noreply.github.com>
2020-12-15 16:11:33 +0530

    Cart Activity created

---
+++ app/src/main/java/com/example/withbash/ui/pay/Cart.java
@@ -0,0 +1,14 @@
+package com.example.withbash.ui.pay;
+import com.example.withbash.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
--- app/src/main/java/com/example/withbash/ui/pay/Pay.java
+++ app/src/main/java/com/example/withbash/ui/pay/Pay.java
@@ -2,11 +2,15 @@ package com.example.withbash.ui.pay;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.withbash.R;
 
@@ -14,9 +18,12 @@ import com.example.withbash.R;
 public class Pay extends Fragment {
 
 
+
     public Pay() {
         // Required empty public constructor
     }
+
+
 
 
     @Override
@@ -24,5 +31,11 @@ public class Pay extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pay, container, false);
+
     }
+
+
+
+
+
 }
